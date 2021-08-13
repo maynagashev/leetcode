b6c891ee1cea714782b96ded06ed8c66db049e87
Evgeniy <lifesup@gmail.com>
2021-08-13 22:07:34 +0700

    852. Peak Index in a Mountain Array (easy) improvement

--- binary-search/852-peak-index-in-a-mountain-array/peak_index_in_mountain_array.go
+++ binary-search/852-peak-index-in-a-mountain-array/peak_index_in_mountain_array.go
@@ -11,12 +11,33 @@ func peakIndexInMountainArray(arr []int) int {
 	end := len(arr) - 1
 	mid := 0
 	for start <= end {
-		mid = start + (end - start)/2
-		if arr[mid]<arr[mid+1] {
+		mid = start + (end-start)/2
+		if arr[mid] < arr[mid+1] {
 			start = mid + 1
 		} else {
 			end = mid - 1
 		}
 	}
 	return start
+}
+
+/*
+Runtime: 8 ms, faster than 81.15% of Go online submissions for Peak Index in a Mountain Array.
+Memory Usage: 4.6 MB, less than 10.66% of Go online submissions for Peak Index in a Mountain Array.
+*/
+func peakIndexInMountainArray2(arr []int) int {
+	start := 0
+	end := len(arr) - 1
+	mid := 0
+	for start <= end {
+		mid = start + (end-start)/2
+		if arr[mid] < arr[mid+1] {
+			start = mid + 1
+		} else if arr[mid] < arr[mid-1] {
+			end = mid - 1
+		} else {
+			return mid
+		}
+	}
+	return -1
 }
