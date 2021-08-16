a31ef11f693967697e42f73334f0034b4cdb6210
Evgeniy <lifesup@gmail.com>
2021-08-16 10:50:22 +0700

    111. Minimum Depth of Binary Tree (easy) without memory leak

--- binary-trees/111-minimum-depth-of-binary-tree/min-depth.go
+++ binary-trees/111-minimum-depth-of-binary-tree/min-depth.go
@@ -1,5 +1,9 @@
 package min_depth
 
+import (
+	"container/list"
+)
+
 type TreeNode struct {
 	Val   int
 	Left  *TreeNode
@@ -13,6 +17,7 @@ type TreeNode struct {
  *     Left *TreeNode
  *     Right *TreeNode
  * }
+ * Possible memory leak when q = q[1:]
  */
 func minDepth(root *TreeNode) int {
 	if root == nil {
@@ -42,6 +47,50 @@ func minDepth(root *TreeNode) int {
 			if node.Right != nil {
 				q = append(q, node.Right)
 			}
+		}
+		depth++
+	}
+
+	return depth
+}
+
+/*
+With proper memory leak handling.
+*/
+func minDepth2(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	q := list.New()
+	q.PushBack(root)
+	depth := 1
+	for q.Len() > 0 {
+		n := q.Len()
+		for i := 0; i < n; i++ {
+
+			// dequeue
+			first := q.Front()
+			node := first.Value
+			q.Remove(first)
+			
+			switch node := node.(type) {
+			case *TreeNode:
+				// fmt.Printf("%+v %+v \n", node.Left, node.Right)
+				// if found any node without children, got a result
+				if node.Left == nil && node.Right == nil {
+					return depth
+				}
+
+				// queue children
+				if node.Left != nil {
+					q.PushBack(node.Left)
+				}
+
+				if node.Right != nil {
+					q.PushBack(node.Right)
+				}
+			}
+
 		}
 		depth++
 	}
