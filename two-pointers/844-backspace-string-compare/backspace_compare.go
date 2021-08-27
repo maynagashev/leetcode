4b05178d0eb67251d36e0b8bb1199596d36f43f6
Evgeniy <lifesup@gmail.com>
2021-08-27 11:07:49 +0700

    + link

--- two-pointers/844-backspace-string-compare/backspace_compare.go
+++ two-pointers/844-backspace-string-compare/backspace_compare.go
@@ -1,7 +1,7 @@
 /*
 Given two strings s and t, return true if they are equal when both are typed into empty text editors. '#' means a backspace character.
-
 Note that after backspacing an empty text, the text will continue empty.
+https://leetcode.com/problems/backspace-string-compare/
 */
 package main
 
